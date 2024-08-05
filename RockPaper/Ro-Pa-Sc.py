81aa4f3b215578283f1db9fa80fd3744d72d63c7
MoyyyL <moyyyl0526@gmail.com>
2024-08-05 17:48:25 -0600

    learning how to use choose

---
+++ RockPaper/Ro-Pa-Sc.py
@@ -0,0 +1,6 @@
+#Get the input
+player = input("Rock-Paper-Scissors: ")
+player = player.lower()
+
+# Computer choose
+RPS = ["rock", "paper", "scissors"]
---
+++ RockPaper/moduleRPS.py
@@ -0,0 +1,4 @@
+from random import choice
+
+RPS = ["rock", "paper", "scissors"]
+print(choice(RPS))
