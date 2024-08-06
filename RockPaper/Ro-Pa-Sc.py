e5bfc6a1874efdb20ecf5cde6fe9ae2a2817e74e
MoyyyL <moyyyl0526@gmail.com>
2024-08-05 19:27:52 -0600

    Rock-paper-scissor-V1

--- RockPaper/Ro-Pa-Sc.py
+++ RockPaper/Ro-Pa-Sc.py
@@ -1,13 +1,33 @@
-from moduleRPS import choosing, winner
+from moduleRPS import choosing, winner, asking
 
-#Get the input
-player = input("Rock-Paper-Scissors: ")
-player = player.lower()
-
-# Computer choose
+# Data
 RPS = ["rock", "paper", "scissors"]
-PC = choosing(RPS)
+pc_points = 0
+player_points = 0
 
-# loking for the winner
-
-winner(player,PC)
+# Getting all
+while True:
+    player = asking(RPS)
+    PC = choosing(RPS)
+    
+    # loking for the winner
+    win = winner(player,PC)
+    print(f"Player: {player}" + "\n" f"PC: {PC}")
+    
+    # Printing winner
+    if win == True:
+        print("You win")
+        player_points += 1
+    elif win == False:
+        print("You lose")
+        pc_points += 1
+    else:
+        print(win)
+    print(f"PC: {pc_points} / You: {player_points}")
+    
+    # again?
+    ask = input("Want to play again?: ").lower()
+    if ask in ["y", "yes", "yeah", "sure"]:
+        pass
+    else:
+        break
--- RockPaper/moduleRPS.py
+++ RockPaper/moduleRPS.py
@@ -1,17 +1,28 @@
 from random import choice
 
+def asking(abc):
+    while True:
+        player = input("Rock-Paper-Scissors: ")
+        player = player.lower()
+        if player in abc:
+            return player
+        else:
+            print("wrong")
+
+
 def choosing(RPSC):
     pc = choice(RPSC)
     return pc
+
 
 def winner(player,pc):
     if player == pc:
         return "Tie"
     elif pc == "scissors" and player == "paper":
-        return "You lose"
+        return False
     elif pc == "paper" and player == "rock":
-        return "you lose"
+        return False
     elif pc == "rock" and player == "scissors":
-        return "you lose"
+        return False
     else:
-        return "You win"
+        return True
