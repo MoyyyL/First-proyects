68a1dbfeb6fdaed30a004bd6a2452f8b0c563344
MoyyyL <moyyyl0526@gmail.com>
2024-08-05 18:23:06 -0600

    adding functions

--- RockPaper/Ro-Pa-Sc.py
+++ RockPaper/Ro-Pa-Sc.py
@@ -1,6 +1,13 @@
+from moduleRPS import choosing, winner
+
 #Get the input
 player = input("Rock-Paper-Scissors: ")
 player = player.lower()
 
 # Computer choose
 RPS = ["rock", "paper", "scissors"]
+PC = choosing(RPS)
+
+# loking for the winner
+
+winner(player,PC)
--- RockPaper/moduleRPS.py
+++ RockPaper/moduleRPS.py
@@ -1,4 +1,17 @@
 from random import choice
 
-RPS = ["rock", "paper", "scissors"]
-print(choice(RPS))
+def choosing(RPSC):
+    pc = choice(RPSC)
+    return pc
+
+def winner(player,pc):
+    if player == pc:
+        return "Tie"
+    elif pc == "scissors" and player == "paper":
+        return "You lose"
+    elif pc == "paper" and player == "rock":
+        return "you lose"
+    elif pc == "rock" and player == "scissors":
+        return "you lose"
+    else:
+        return "You win"
