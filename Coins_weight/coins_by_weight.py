44291a9d3cf42c08983d67e7d9c2c5781b39d524
MoyyyL <moyyyl0526@gmail.com>
2024-08-07 17:49:25 -0600

    adding .name to the class coin

--- Coins_weight/coins_by_weight.py
+++ Coins_weight/coins_by_weight.py
@@ -1,24 +1,32 @@
 # Data:
 class Coins:
-    def __init__(self, weight, wrapper):
+    def __init__(self, weight, wrapper, name):
         self.weight = weight
         self.wrapper = wrapper
+        self.name = name
 
-penny = Coins(2.500, 50)
-nickel = Coins(5.000, 40)
-dime = Coins(2.268, 50)
-quarter = Coins(5.670, 40)
+penny = Coins(2.500, 50, "penny")
+nickel = Coins(5.000, 40, "nickel")
+dime = Coins(2.268, 50, "dime")
+quarter = Coins(5.670, 40, "quarter")
 
-coins = ["pennys", "nickels", "dimes", "quarters"]
+coins = [penny, nickel, dime, quarter]
 weight = []
+amount_of_wrappers = []
+
 
 # get input
 i = 0
 while i < len(coins):
     try:
-        how_many = int(input(f"Weight of your {coins[i]}: "))
+        how_many = int(input(f"Weight of your {coins[i].name}s: "))
         weight.append(how_many)
         i += 1
     except:
         print("i asked for a number")
 
+# work the input
+
+for i in range(len(weight)):
+    coin_count = weight[i] / coins[i].weight
+    print(coin_count)
